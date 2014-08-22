6126c10606093d47fde6e4033e09d87a7a874047
n2o <cmeter@googlemail.com>
2014-08-22 21:49:08 +0200

    Add option to add articles

--- plugins/articleadd/deploy/render.php
+++ plugins/articleadd/deploy/render.php
@@ -62,7 +62,7 @@
   </div>
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
-      <input type="button" id="button" class="btn btn-default" value="Wow">
+      <input type="button" id="button" class="btn btn-default" value="Artikel hinzufügen">
     </div>
   </div>
 </form>
--- plugins/articleadd/deploy/todb.php
+++ plugins/articleadd/deploy/todb.php
@@ -1,6 +1,4 @@
 <?php
-var_dump($_POST);
-print(getcwd());
 # Get variables
 $name = $_POST['name'];
 $description = $_POST['description'];
@@ -12,23 +10,30 @@ $channel = $_POST['channel'];
 $price = $_POST['price'];
 $quantity = $_POST['quantity'];
 
-# Include app.php for DB connection
-include("../../../app.php");
+# Check for required fields
+if (empty($name) OR empty($description)) {
+  print("<div class='alert alert-danger' role='alert'>Bitte alle erforderlichen Felder ausfüllen.</div>");
+} else {
+  # Include app.php for DB connection
+  include("../../../app.php");
 
-$db = DB::get();
+  $db = DB::get();
 
-$res = $db->prepare("INSERT INTO `Articles` 
-  ('Name', 'Description', 'Transmission', 'Category', 'Subcategory', 'Weight', 'Channel', 'Price', 'Quantity')
-  VALUES
-  (:name, :description, :transmission, :category, :subcategory, :weight, :channel, :price, :quantity)");
-$res->bindParam(':name', $name, PDO::PARAM_STR, strlen($name));
-$res->bindParam(':description', $description, PDO::PARAM_STR, strlen($description));
-$res->bindParam(':transmission', $transmissions, PDO::PARAM_STR, strlen($transmission));
-$res->bindParam(':category', $category, PDO::PARAM_STR, strlen($category));
-$res->bindParam(':subcategory', $subcategory, PDO::PARAM_STR, strlen($subcategory));
-$res->bindParam(':weight', $weight, PDO::PARAM_INT);
-$res->bindParam(':channel', $channel, PDO::PARAM_STR, strlen($channel));
-$res->bindParam(':price', $price, PDO::PARAM_STR, strlen($price));
-$res->bindParam(':quantity', $quantity, PDO::PARAM_STR, strlen($quantity));
-$res->execute();
+  $res = $db->prepare("INSERT INTO `Articles` 
+    (Name, Description, Transmission, Category, Subcategory, Weight, Channel, Price, Quantity)
+    VALUES
+    (:name, :description, :transmission, :category, :subcategory, :weight, :channel, :price, :quantity)");
+  $res->bindParam(':name', $name, PDO::PARAM_STR, strlen($name));
+  $res->bindParam(':description', $description, PDO::PARAM_STR, strlen($description));
+  $res->bindParam(':transmission', $transmissions, PDO::PARAM_STR, strlen($transmission));
+  $res->bindParam(':category', $category, PDO::PARAM_STR, strlen($category));
+  $res->bindParam(':subcategory', $subcategory, PDO::PARAM_STR, strlen($subcategory));
+  $res->bindParam(':weight', $weight, PDO::PARAM_INT);
+  $res->bindParam(':channel', $channel, PDO::PARAM_STR, strlen($channel));
+  $res->bindParam(':price', $price, PDO::PARAM_STR, strlen($price));
+  $res->bindParam(':quantity', $quantity, PDO::PARAM_STR, strlen($quantity));
+  $res->execute();
+
+  print("<div class='alert alert-success' role='alert'>Artikel erfolgreich hinzugefügt.</div>");
+}
 ?>
